6128db58d5289aff4199499aadf88a17037b0438
dangphuchuu <phuchuu0120@gmail.com>
2023-08-26 11:53:10 +0700

    first commit

---
+++ app/Http/Controllers/ShoesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shoes;
+use Illuminate\Http\Request;
+
+class ShoesController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+    public function all_shoes()
+    {
+        $shoes = Shoes::all();
+        return response()->json([
+            'shoes' => $shoes,
+            'status' => 200,
+            'message' => 'All Shoes'
+        ]);
+    }
+    public function shoes($id)
+    {
+        $shoes = Shoes::find($id);
+        return response()->json([
+            'shoes' => $shoes,
+            'status' => 200,
+            'message' => 'Success'
+        ]);
+    }
+    public function add(Request $request)
+    {
+        $shoes = new Shoes([
+            'image' => $request->image,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'color' => $request->color
+        ]);
+        $shoes->save();
+        if ($shoes) {
+            return response()->json([
+                'Message' => 'Added Sucessfully'
+            ]);
+        } else {
+            return response()->json([
+                'Message' => 'Added Failled'
+            ]);
+        }
+    }
+}
